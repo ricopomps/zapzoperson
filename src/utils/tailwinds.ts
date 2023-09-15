19fb2e78a9cddc5351228b8552715e1689cb792f
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-15 10:51:26 -0300

    feat: Responsive chat layout

---
+++ src/utils/tailwinds.ts
@@ -0,0 +1,8 @@
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../tailwind.config";
+
+const twConfig = resolveConfig(tailwindConfig);
+
+const mdBreakpoint = Number.parseInt((twConfig.theme?.screens as any).md);
+
+export { mdBreakpoint };
